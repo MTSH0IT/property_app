f9fe7d446a4a7212beb74db486f34653c4fa957f
MTSH <malshurbaji.11@gmail.com>
2025-05-04 18:24:04 +0300

    property_app init

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const PropertyApp());
+}
+
+class PropertyApp extends StatelessWidget {
+  const PropertyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Scaffold());
+  }
+}
